0cdbbd032d2dbfbe345a7521c75d9569e40c66e5
Adriano Nascimento <adr.nasc@gmail.com>
2026-04-13 18:34:15 -0300

    Verifica se o cpf já tá cadastrado

--- src/main/java/br/com/veiculo/repository/ClienteRepository.java
+++ src/main/java/br/com/veiculo/repository/ClienteRepository.java
@@ -3,4 +3,7 @@ package br.com.veiculo.repository;
 import br.com.veiculo.entity.*;
 import org.springframework.data.jpa.repository.*;
 
-public interface ClienteRepository extends JpaRepository<Cliente,Long>{}
+public interface ClienteRepository extends JpaRepository<Cliente,Long>{
+    boolean existsByCpf(String cpf);
+    boolean existsByCpfAndIdNot(String cpf, Long id);
+}
